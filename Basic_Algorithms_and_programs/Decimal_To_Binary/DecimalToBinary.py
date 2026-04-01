ce13b7e4640ae5ed59be20a496edb3623249e024
LOKESH A D <aakulalokesh2007@gmail.com>
2026-04-01 21:37:59 +0530

    Add files via upload

---
+++ Basic_Algorithms_and_programs/Decimal_To_Binary/DecimalToBinary.py
@@ -0,0 +1,12 @@
+def DecimalToBinary(num):
+    
+    if num > 1:
+        DecimalToBinary(num // 2)
+    
+    
+    print(num % 2, end='')
+
+if __name__ == '__main__':
+    dec_val = int(input("Enter your number: "))
+    DecimalToBinary(dec_val)
+     
