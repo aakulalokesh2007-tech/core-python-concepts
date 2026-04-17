ba33e96a087186ff2810db872c4d5a204c67ded6
LOKESH A D <aakulalokesh2007@gmail.com>
2026-04-17 21:02:26 +0530

    Add files via upload

---
+++ pro-tips/Update_a_Dictionary/Method_3/Method_3.py
@@ -0,0 +1,10 @@
+# shortcut in dictionary
+
+default={"name":"N/A","phone":"N/A"}
+
+user_data={"name":"lokesh","phone":"1234567890"}
+
+default |= user_data  #this will update the dict
+print(default)
+
+
