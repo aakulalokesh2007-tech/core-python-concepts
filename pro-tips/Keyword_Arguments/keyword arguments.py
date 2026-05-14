ead8492e8350a95fe284baf2b46f9fdaccb8a3e4
LOKESH A D <aakulalokesh2007@gmail.com>
2026-05-14 05:56:34 +0530

    Add files via upload

---
+++ pro-tips/Keyword_Arguments/keyword arguments.py
@@ -0,0 +1,7 @@
+# Using Keyword Arguments
+def greet(first, middle, last):
+   
+    print('Hello ' + first + ' ' + middle + ' ' + last)
+
+# Calling the function with names in a different order
+greet(middle='arugument', first='keyword', last='is nice')
